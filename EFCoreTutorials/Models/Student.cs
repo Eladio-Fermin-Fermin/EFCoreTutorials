c57fb93c0331f8da15c478e34234c856043ce1b0
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-02-17 09:15:23 -0400

    Se creo la carpeta Models y dentro 2 clases Student y Grade.

---
+++ EFCoreTutorials/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutorials.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public int? GradeId { get; set; }
+        public Grade Grade { get; set; }
+
+        public Student()
+        {
+            StudentId = 0;
+            Name = string.Empty;
+            GradeId = 0;
+        }
+    }
+}
